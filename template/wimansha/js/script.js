7ce6decfa9f1e622a5f0a5764e073331e0179d8b
wimanshaherath <wimanshah@gmail.com>
2020-10-02 11:55:04 +0530

    Added a new blog template

---
+++ template/wimansha/js/script.js
@@ -0,0 +1,60 @@
+var txtType=function(el,toRotate,period){
+    this.el=el;
+    this.toRotate=toRotate;
+    this.period=parseInt(period,10) || 2000;
+    this.loopNum=0;
+    this.txt='';
+    this.tick();
+    this.isDeleting=false;
+
+}
+
+txtType.prototype.tick=function()
+{
+    var i=this.loopNum%this.toRotate.length;
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewriter');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new txtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewriter > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
